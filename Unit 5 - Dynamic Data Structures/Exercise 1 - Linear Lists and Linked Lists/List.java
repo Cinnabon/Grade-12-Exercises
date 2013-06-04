ae263ee77c15631c544626440709658fd61878ac
Clement Hoang <clement.hoang24@gmail.com>
2013-06-04 15:44:02 -0300

    Update List.java

--- Unit 5 - Dynamic Data Structures/Exercise 1 - Linear Lists and Linked Lists/List.java
+++ Unit 5 - Dynamic Data Structures/Exercise 1 - Linear Lists and Linked Lists/List.java
@@ -1,6 +1,50 @@
 
 public class List {
-  private Fraction head;
+	private Fraction head;
+	/**
+	 * will insert a fraction at the end of the Linked List
+	 * @param f
+	 */
+	public void insertFirst(Fraction f) {
+		head = f;
+	}
+	
+	/**
+	 * will insert a second fraction at the end of the Linked List
+	 * @param f
+	 */
+	public void insertSecond(Fraction f) {
+		head.link = f;
+	}
+	
+	/**
+	 * will insert a third fraction at the end of the Linked List
+	 * @param f
+	 */
+	public void insertThird(Fraction f) {
+		head.link.link = f;
+	}
+	
+	/**
+	 * will insert a fraction at the end of the linked list
+	 * @param f
+	 */
+	public void addatEnd(Fraction f) {
+		Fraction temp = head;
+		while (temp!=null){
+			temp = temp.link;
+		}
+		temp = f;
+		//add at front then reverse list
+		
+	}
+	/*
+	 * for (student temp = head; temp != null; temp= temp.link){
+		System.out.println(temp.mark);
+		//output contents of object using temp
+	}
+
+	 */
 	class Fraction {
 		int num;
 		int den;
@@ -12,11 +56,12 @@ public class List {
 			link = null;
 		}
 		
-		Fraction(int num, int den, Fraction f) {
-			this.num = num;
-			this.den = den;
+		Fraction(Fraction f, int n, int d) {
+			this.num = n;
+			this.den = d;
 			link = f;
 		}
+		
 	}
 	
 }
